4830e314511c1082f6b9ad6a0d010ca75a8803b2
Ranit Naha <naharanit123@gmail.com>
2024-03-07 21:36:35 +0530

    feat/shifting-X to O

--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -1,23 +1,33 @@
+import { useState } from "react";
 import Pieces from "./Pieces";
 
 const Box = () => {
+  const [val, setVal] = useState(Array(9).fill(null));
+  const [turn, setTurn] = useState(true);
+
+  function handleClick(index) {
+    turn ? console.log("X", index) : console.log("0", index);
+    setTurn(!turn);
+    console.log(turn);
+  }
+
   return (
     <>
       <div className="">
         <div className="flex justify-center text-center">
-          <Pieces nums="1" />
-          <Pieces nums="2" />
-          <Pieces nums="3" />
+          <Pieces onButtonClick={() => handleClick(0)} nums={val[0]} />
+          <Pieces onButtonClick={() => handleClick(1)} nums={val[1]} />
+          <Pieces onButtonClick={() => handleClick(2)} nums={val[2]} />
         </div>
         <div className="flex justify-center text-center">
-          <Pieces nums="4" />
-          <Pieces nums="5" />
-          <Pieces nums="6" />
+          <Pieces onButtonClick={() => handleClick(3)} nums={val[3]} />
+          <Pieces onButtonClick={() => handleClick(4)} nums={val[4]} />
+          <Pieces onButtonClick={() => handleClick(5)} nums={val[5]} />
         </div>
         <div className="flex justify-center text-center">
-          <Pieces nums="7" />
-          <Pieces nums="8" />
-          <Pieces nums="9" />
+          <Pieces onButtonClick={() => handleClick(6)} nums={val[6]} />
+          <Pieces onButtonClick={() => handleClick(7)} nums={val[7]} />
+          <Pieces onButtonClick={() => handleClick(8)} nums={val[8]} />
         </div>
       </div>
     </>
--- src/components/Pieces.jsx
+++ src/components/Pieces.jsx
@@ -1,15 +1,19 @@
 import PropTypes from "prop-types";
 
-const Pieces = ({ nums }) => {
+const Pieces = ({ onButtonClick, nums }) => {
   return (
-    <div className="flex justify-center items-center border border-sky-500 h-20 w-20">
+    <div
+      onClick={onButtonClick}
+      className="flex justify-center items-center border border-sky-500 h-20 w-20"
+    >
       {nums}
     </div>
   );
 };
 
 Pieces.propTypes = {
-  nums: PropTypes.any.isRequired, // Use the appropriate prop type
+  nums: PropTypes.bool,
+  onButtonClick: PropTypes.func.isRequired, // Use the appropriate prop type
 };
 
 export default Pieces;
